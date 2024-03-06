c5fb5471365b6e7940a98e40ed0c249a0b543c00
batumento <batumento@192.168.1.11>
2024-03-06 19:26:21 +0300

    Added countdown timer and learned closures

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var labelName: UILabel!
+    let eggTimes = ["Soft": 5, "Medium": 420, "Hard": 720]
 
+    var secondsRemaining:Int = 0
+    var timer = Timer()
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle!)
+        timer.invalidate()
+        secondsRemaining = eggTimes[sender.currentTitle!]!
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if self.secondsRemaining > 0
+                {
+                    print ("\(self.secondsRemaining) seconds")
+                    self.secondsRemaining -= 1
+                }
+                else
+                {
+                    self.labelName.text = "Finished"
+                    Timer.invalidate()
+                }
+            }
     }
     
 }
