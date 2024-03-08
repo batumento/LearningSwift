b3214ef6c70e626d5fca9c081ff611b920d1ad47
batumento <batuhanavniemiroglu@gmail.com>
2024-03-08 21:09:55 +0300

    preparation for quizzler

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    var quiz = ["2 + 2 = 4": "True", "4 + 4 = 9": "False", "3 + 1 = 4": "True"]
+    var anyQuestion: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        anyQuestion = quiz.randomElement()?.key
+        questionLabel.text = anyQuestion
     }
 
-
+    @IBAction func answerButton(_ sender: UIButton) {
+        sender.isUserInteractionEnabled = false
+        if sender.currentTitle! == quiz[anyQuestion!]{
+            questionLabel.text = "Correct"
+            sender.backgroundColor = UIColor.green
+        }
+        else
+        {
+            questionLabel.text = "Wrong"
+            sender.backgroundColor = UIColor.red
+        }
+    }
 }
 
