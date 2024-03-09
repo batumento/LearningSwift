9d76ee29ab6f61b48616d9d1653754dd05a21ab6
batumento <batuhanavniemiroglu@gmail.com>
2024-03-09 17:18:29 +0300

    completed

--- Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -11,59 +11,47 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
 
-    var quiz = [Question(q: "A slug's blood is green.", a: "True"),
-                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-                Question(q: "Google was originally called 'Backrub'.", a: "True"),
-                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-]
-    var questionNumber: Int = 0
+    var quizBrain = QuizBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-    //You should learn index of random element 
-    @IBAction func answerButton(_ sender: UIButton) {
-        let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[questionNumber]
-        if userAnswer == actualAnswer.answer {
+    @IBAction func answerButton(_ sender: UIButton)
+    {
+        let userAnswer = sender.currentTitle!
+        let checkedAnswer = quizBrain.checkAnswer(uAnswer: userAnswer)
+        if  checkedAnswer
+        {
+            quizBrain.incraseScore()
             sender.backgroundColor = UIColor.green
-            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(clearButton), userInfo: nil, repeats: false)        }
+            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(clearButton), userInfo: nil, repeats: false)
+        }
         else
         {
             sender.backgroundColor = UIColor.red
             Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(clearButton), userInfo: nil, repeats: false)
         }
-        if questionNumber < quiz.indices.last! {
-            questionNumber += 1
-        }
-        else
-        {
-            questionNumber = 0
-        }
+        quizBrain.nextQuestion()
         updateUI()
     }
+
     func updateUI()
     {
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
-        questionLabel.text = quiz[questionNumber].text
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        progressBar.progress = quizBrain.remainingQuestionBar()
+        questionLabel.text = quizBrain.getQuestion()
     }
+
     @objc func clearButton()
     {
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
     }
 }
-
